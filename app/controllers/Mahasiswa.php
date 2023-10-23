68552e69921c9a1b93c49cda029ce66109adced7
litayusdiafatimah <litayusdiaf123@gmail.com>
2023-10-23 11:19:40 +0700

    Video #11 Delete Data

--- app/controllers/Mahasiswa.php
+++ app/controllers/Mahasiswa.php
@@ -35,6 +35,22 @@ class Mahasiswa extends Controller {
 
     }
 
+    public function hapus($id) 
+    {
+        if ($this->model('Mahasiswa_model')->hapusDataMahasiswa($id) > 0)
+        {
+            Flasher::setFlash('berhasil', 'dihapus', 'success');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        } else {
+            Flasher::setFlash('gagal', 'dihapus', 'danger');
+            header('Location: ' . BASEURL . '/mahasiswa');
+            exit;
+        }
+        $this->index();
+
+    }
+
 
     
 }
